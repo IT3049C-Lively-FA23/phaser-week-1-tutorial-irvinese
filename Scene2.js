91253e39b3bcbf58d327bcaa8cb673643faccd02
Shane Irvine <113709537+irvinese@users.noreply.github.com>
2024-03-08 11:28:54 -0500

    ran through Github to fix issues with OnDestory, made it a separate method

--- Scene2.js
+++ Scene2.js
@@ -1,42 +1,42 @@
-class Scene2 extends Phaser.Scene{
-  constructor(){
+class Scene2 extends Phaser.Scene {
+  constructor() {
     super("playGame");
   }
 
-  create(){
-    this.background = this.add.titleSprite(0,0, config.width, config.height, "background");
-    this.background.setOrigin(0,0);
+  create() {
+    this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
+    this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
-    this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2")
-    this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3")
+    this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
+    this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
+    this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");
 
     this.anims.create({
       key: "ship1_anim",
       frames: this.anims.generateFrameNumbers("ship"),
       frameRate: 20,
-      repeate: -1
+      repeat: -1
     });
 
     this.anims.create({
       key: "ship2_anim",
       frames: this.anims.generateFrameNumbers("ship2"),
       frameRate: 20,
-      repeate: -1
+      repeat: -1
     });
 
     this.anims.create({
       key: "ship3_anim",
       frames: this.anims.generateFrameNumbers("ship3"),
       frameRate: 20,
-      repeate: -1
+      repeat: -1
     });
 
     this.anims.create({
       key: "explode",
       frames: this.anims.generateFrameNumbers("explosion"),
       frameRate: 20,
-      repeate: 0,
+      repeat: 0,
       hideOnComplete: true
     });
 
@@ -46,17 +46,17 @@ class Scene2 extends Phaser.Scene{
 
     this.input.on('gameobjectdown', this.destroyShip, this);
 
-    this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"})
+    this.add.text(20, 20, "Playing game", { font: "25px Arial", fill: "yellow" });
   }
 
-  moveShip(ship, speed){
+  moveShip(ship, speed) {
     ship.y += speed;
-    if(ship.y > config.height){
+    if (ship.y > config.height) {
       this.resetShipPos(ship);
     }
   }
 
-  resetShipPos(ship){
+  resetShipPos(ship) {
     ship.y = 0;
     var randomX = Phaser.Math.Between(0, config.width);
     ship.x = randomX;
@@ -67,11 +67,11 @@ class Scene2 extends Phaser.Scene{
     this.moveShip(this.ship2, 2);
     this.moveShip(this.ship3, 3);
 
-    destroyShip(pointer, gameObject){
-      gameObject.setTexture("explosion");
-      gameObject.play("explode");
-    }
+    this.background.tilePositionY -= 0.5;
+  }
 
-    this.background.titlePositionY -= 0.5;
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture("explosion");
+    gameObject.play("explode");
   }
 }
