4974040fe754e9e6c743047c84d1f33e31a40e10
Shane Irvine <113709537+irvinese@users.noreply.github.com>
2024-03-08 15:32:13 -0500

    Finished part 6 of the playlist

--- Scene2.js
+++ Scene2.js
@@ -40,6 +40,45 @@ class Scene2 extends Phaser.Scene {
       hideOnComplete: true
     });
 
+    this.anims.create({
+      key:"red",
+      frames:this.anims.generateFrameNumbers("power-up", {
+        start: 0,
+      end: 1
+          }),
+          frameRate: 20,
+          repeat: -1
+    });
+
+    this.anims.create({
+      key: "gray",
+      frames: this.anims.generateFrameNumbers("power-up", {
+        start:2,
+        end: 3
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.powerUps = this.physics.add.group();
+
+    var maxObjects = 4;
+    for(var i = 0; i <= maxObjects; i++){
+      var powerUp = this.physics.add.sprite(16,16, "power-up");
+      this.powerUps.add(powerUp);
+      powerUp.setRandomPosition(0,0, game.config.width, game.config.heigt);
+
+      if (Math.random() > 0.5){
+        powerUp.play("red");
+      }else{
+        powerUp.play("grey");
+      }
+
+      powerUp.serVelocity(100, 100);
+      powerUp.setColliderWorldBounds(true);
+      powerUp.setBounce(1);
+    }
+
     this.ship1.setInteractive();
     this.ship2.setInteractive();
     this.ship3.setInteractive();
--- game.js
+++ game.js
@@ -2,10 +2,20 @@ var config = {
   width: 256,
   height: 272,
   backgroundColor: 0x000000,
-  scene: [Scene1, Scene2]
+  scene: [Scene1, Scene2],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade:{
+      debug: false
+    }
+  }
 }
+
+var game = new Phaser.Game(config);
 
 window.onload = function()
 {
-  var game = new Phaser.Game();
+  var game = new Phaser.Game(config);
 }
+
